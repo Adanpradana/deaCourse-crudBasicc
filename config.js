96ba5a168e3939b48bec9ef3bd728eb4a6a1df07
fullSnack <100656834+Adanpradana@users.noreply.github.com>
2022-05-21 00:34:14 +0700

    ADD REPO

---
+++ config.js
@@ -0,0 +1,17 @@
+// import sql
+const mysql = require("mysql");
+
+// database credential
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "dota_2",
+});
+
+// connect/intergrate to mysql
+connection.connect((error) => {
+  if (error) throw error;
+  console.log("DATABASE CONNECTED");
+});
+module.exports = connection;
